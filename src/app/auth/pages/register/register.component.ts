831355142cd31b0779abf91e249ef2e6435332f0
padrocha <p.roch@outlook.es>
2021-06-30 22:15:52 -0500

    valid & invalid

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { AuthService } from '@auth/services/auth/auth.service';
 
 @Component({
@@ -11,19 +11,29 @@ export class RegisterComponent implements OnInit {
   register: FormGroup;
   confirme: FormControl;
   constructor() {
+
     this.register = new FormGroup({
       nickname: new FormControl('', Validators.required),
       email: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
     });
-    this.confirme = new FormControl('')
-   }
+    this.confirme = new FormControl('', Validators.required)
+  }
 
   ngOnInit(): void {
   }
-  passConfirmed(): boolean {
-    return this.confirme.touched && this.register.get('password').value == this.confirme.value && this.confirme.valid;
+
+  isInvalid(): boolean {
+    return this.confirme.touched &&
+      this.register.get('password').value !== this.confirme.value &&
+      this.register.get('password').valid &&
+      this.confirme.valid;
   }
 
-
+  isValid(): boolean {
+    return this.confirme.touched &&
+      this.register.get('password').value === this.confirme.value &&
+      this.register.get('password').valid &&
+      this.confirme.valid;
+  }
 }
